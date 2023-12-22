a9a1068234126afde65cde7a327c633d32dc80a6
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-12-22 22:20:28 +0530

    user read and delete implemented

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,9 @@
-const User  = require('../models/userModel.js')
+const User = require('../models/userModel.js')
 exports.home = (req, res) => {
     res.send("Welcome to app")
 }
 
-exports.createUser =async (req, res) => {
+exports.createUser = async (req, res) => {
     try {
         const { name, email } = req.body;
         console.log(name)
@@ -12,12 +12,12 @@ exports.createUser =async (req, res) => {
             throw new Error('name and email are required')
         }
         // check if user is already registered
-        const userExists =await User.findOne({email})
+        const userExists = await User.findOne({ email })
         console.log(userExists);
         if (userExists) {
             throw new Error('User already registered')
         }
-        const user =await User.create({ name, email }) //command to write document in collection in mongo db
+        const user = await User.create({ name, email }) //command to write document in collection in mongo db
         res.status(201).json({
             sucess: true,
             message: `${name} user created sucessfully`,
@@ -29,6 +29,37 @@ exports.createUser =async (req, res) => {
         res.status(400).json({
             sucess: false,
             message: error.message
+        })
+    }
+}
+
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({"sucess" : true,
+            users})
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({
+            "sucess": false,
+            "message": error.message
+        })
+    }
+}
+exports.deleteUser = async (req, res) => {
+    try {
+        const userId= req.params.id;
+        console.log(userId)
+        const userDeleted = await User.findByIdAndDelete(userId)
+        res.status(200).json({"sucess" : true,
+            "message":"user deleted"})
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({
+            "sucess": false,
+            "message": error.message
         })
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express= require("express")
 const router = express.Router()
-const {home,createUser}=require('../controllers/userControllers')
+const {home,createUser,getUsers,deleteUser}=require('../controllers/userControllers')
 
 router.get("/",home)
+router.get("/getusers",getUsers)
 router.post("/createuser",createUser)
+router.delete("/deleteuser/:id",deleteUser)
 module.exports=router
